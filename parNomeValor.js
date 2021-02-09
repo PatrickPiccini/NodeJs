6e827a85c2e52371b6e23983f72c74a7d84b9306
Patrick Piccini <patrickbpiccini@hotmail.com>
2021-02-08 23:15:30 -0300

    first commit!

---
+++ parNomeValor.js
@@ -0,0 +1,24 @@
+
+//par nome/valor
+
+const c = 'Hello World'
+
+
+function FuExec(){
+    const c = 'teste de nodejs!'
+    return c
+}
+
+const cliente = {
+    nome: "patrick",
+    idade: 21,
+    peso: 71,
+    endereço: {
+        casa: 'av alceu laus',
+        numero: 792,
+    }   
+}
+
+console.log(c)
+console.log(FuExec())
+console.log(cliente)
